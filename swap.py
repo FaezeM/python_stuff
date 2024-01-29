47b074cadb65f62bd4b74b089d84b5d49004d83f
FaezeM <f.memari76@gmail.com>
2024-01-29 20:43:00 +0100

    swap

---
+++ swap.py
@@ -0,0 +1,7 @@
+a = input("Please enter a:\n")
+b = input("Please enter b:\n")
+temp = a
+a = b
+b = temp
+print(f"The initial value of 'a' was {b} and now is {a}.\nThe initial value of 'b' was {a} and now is {b}.")
+input("Press any key to exit...")
